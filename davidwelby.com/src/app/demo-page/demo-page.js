c26a1f3574f879aaf927d1ffcedf5f75becb21cc
davidwelby <dwelby101@msn.com>
2016-02-25 06:12:49 +0000

    updated homepage styling and homepage content

--- davidwelby.com/src/app/demo-page/demo-page.js
+++ davidwelby.com/src/app/demo-page/demo-page.js
@@ -39,6 +39,44 @@ $win.resize(getWidth).mousemove(function(e) {
     //set header colour to dynamic bg colour
     $('#header').css('background','rgba('+ rgb1.join(',')+')' );
 
-
 }).resize();
+
+/***********************************************
+ * Function for the responsive menu
+ ***********************************************/
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+            if (classes[i] === className) {
+                classes.splice(i, 1);
+                break;
+            }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        //toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    };
+
+}(this, this.document));
 
